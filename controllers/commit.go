6e2225b8d76dd0ac7c6543bd879fcf7ff9b63ec6
Jamie Mansfield <dev@jamierocks.uk>
2016-09-18 22:57:37 +0100

    Short branch name, short commit id and git.io

--- controllers/commit.go
+++ controllers/commit.go
@@ -8,6 +8,9 @@ import (
     "gopkg.in/macaron.v1"
     "strconv"
     "strings"
+    "net/http"
+    "log"
+    "net/url"
 )
 
 func GetWebhook(ctx *macaron.Context) {
@@ -17,14 +20,32 @@ func GetWebhook(ctx *macaron.Context) {
         var res github.PushEvent
         json.Unmarshal(body, &res)
 
+        branch := strings.Split(*res.Ref, "/")[2]
+        compare := shortenUrl(*res.Compare)
+
         modules.BOT.ChannelMessageSend(modules.CONFIG.Section("DISCORD").Key("channel").String(),
-            "[" + *res.Repo.Name + "] " + *res.Pusher.Name + " pushed " + strconv.Itoa(len(res.Commits)) + " commits to " + *res.Ref + " " + *res.Compare)
+            "[" + *res.Repo.Name + "] " + *res.Pusher.Name + " pushed " + strconv.Itoa(len(res.Commits)) + " commits to " + branch + " " + compare)
 
         for _, commit := range res.Commits {
             message := strings.Split(*commit.Message, "\n")[0]
+            id := (*commit.ID)[0:8]
 
             modules.BOT.ChannelMessageSend(modules.CONFIG.Section("DISCORD").Key("channel").String(),
-                *res.Repo.Name + "/" + *res.Ref + " " + *commit.ID + ": " + message + " (By " + *commit.Author.Name + ")")
+                *res.Repo.Name + "/" + branch + " " + id + ": " + message + " (By " + *commit.Author.Name + ")")
         }
     }
+}
+
+func shortenUrl(longUrl string) string {
+    values := url.Values{
+        "url": { longUrl },
+    }
+    response, err := http.PostForm("https://git.io", values)
+    if err != nil {
+        log.Println("failed to shorten url")
+        return longUrl
+    }
+    shortUrl := response.Header.Get("Location")
+    defer response.Body.Close()
+    return shortUrl
 }
