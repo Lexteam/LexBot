9391fe12fa4905527f29dff2638343febec6e974
Jamie Mansfield <dev@jamierocks.uk>
2016-09-19 11:10:24 +0100

    Lets neaten this up

--- controllers/commit.go
+++ controllers/commit.go
@@ -1,16 +1,13 @@
 package controllers
 
 import (
+    "strconv"
     "io/ioutil"
     "encoding/json"
     "github.com/google/go-github/github"
     "github.com/lexteam/lexbot/modules"
+    "github.com/lexteam/lexbot/utils"
     "gopkg.in/macaron.v1"
-    "strconv"
-    "strings"
-    "net/http"
-    "log"
-    "net/url"
 )
 
 func GetWebhook(ctx *macaron.Context) {
@@ -20,32 +17,18 @@ func GetWebhook(ctx *macaron.Context) {
         var res github.PushEvent
         json.Unmarshal(body, &res)
 
-        branch := strings.Split(*res.Ref, "/")[2]
-        compare := shortenUrl(*res.Compare)
+        branch := utils.GetBranchName(*res.Ref)
+        compare := utils.GetGitioUrl(*res.Compare)
 
         modules.BOT.ChannelMessageSend(modules.CONFIG.Section("DISCORD").Key("channel").String(),
             "[" + *res.Repo.Name + "] " + *res.Pusher.Name + " pushed " + strconv.Itoa(len(res.Commits)) + " commits to " + branch + " " + compare)
 
         for _, commit := range res.Commits {
-            message := strings.Split(*commit.Message, "\n")[0]
-            id := (*commit.ID)[0:8]
+            message := utils.GetShortCommitMessage(*commit.Message)
+            id := utils.GetShortCommitID(*commit.ID)
 
             modules.BOT.ChannelMessageSend(modules.CONFIG.Section("DISCORD").Key("channel").String(),
                 *res.Repo.Name + "/" + branch + " " + id + ": " + message + " (By " + *commit.Author.Name + ")")
         }
     }
-}
-
-func shortenUrl(longUrl string) string {
-    values := url.Values{
-        "url": { longUrl },
-    }
-    response, err := http.PostForm("https://git.io", values)
-    if err != nil {
-        log.Println("failed to shorten url")
-        return longUrl
-    }
-    shortUrl := response.Header.Get("Location")
-    defer response.Body.Close()
-    return shortUrl
 }
